19f6ee37e831f642be81666cbda2d3efa5ae6aa0
arman-bd <arman.hossain.bd93@gmail.com>
2022-11-17 15:53:40 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# gulpy-the-bot
-A gulper.io Bot developed using Python and Selenium Web Driver
+# Gulpy the Gulper.io Bot
+
+**Gulpy** is a bot for [Gulper.io](https://gulper.io) that is designed to be a simple, easy-to-use, and easy-to-extend bot. It is written in Python and uses Selenium to interact with the Gulper.io website.
+
+## Installation
+
+**Gulpy** requires Python 3 or higher and [ChromeDriver](https://chromedriver.chromium.org/downloads). It also requires the following Python packages:
+
+  * [selenium](https://pypi.org/project/selenium/)
+
+Start by installing the dependencies:
+
+> pip install -r requirements.txt
+
+Then, download the appropriate version of [ChromeDriver](https://chromedriver.chromium.org/downloads) based on your system's Chrome Browser version and place it in the same directory as **Gulpy**.
+
+## Usage
+
+**Gulpy** is designed to be run from the command line. It can be run with the following command:
+
+> python gulpy.py
+
+## Gulpy the Gulper.io Bot in Action
+
+**Home Page**
+<img src="/screenshots/screenshot_001.png" width="500" />
+
+**Updating Name**
+<img src="/screenshots/screenshot_002.png" width="500" />
+
+**Moving**
+<img src="/screenshots/screenshot_003.png" width="500" />
+
+**Eating**
+<img src="/screenshots/screenshot_004.png" width="500" />
+
+
+### Disclaimer
+
+This bot is not affiliated with Gulper.io in any way. It is designed to be used for educational purposes only. The very purpose of this bot is to demonstrate how to use Selenium to interact with a website. Use at your own risk. I am not responsible for any bans or other issues that may arise from using this bot.
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ gulpy.py
@@ -0,0 +1,140 @@
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Prepare Selenium Driver Options
+options = Options()
+# options.add_argument('--headless')  # Run Selenium Driver in Headless Mode
+# options.add_argument('--no-sandbox') # No Sandbox for Selenium Driver
+# options.add_argument('--disable-dev-shm-usage') # Disable Dev Shm Usage for Selenium Driver
+
+# Create Selenium Driver
+driver: webdriver.Chrome = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=options)
+
+# Initialize Action Chains
+action_chains: ActionChains = ActionChains(driver)
+
+# Set Window Size [ 800 x 800 ]
+driver.set_window_position(0, 0)
+driver.set_window_size(800, 800)
+
+
+# Preference Flags
+set_once = False
+
+
+def the_gulper_bot(driver: webdriver.Chrome):
+    """The Gulper Bot: A bot that gulps the blobs in the game gulper.io
+
+    Args:
+        driver (webdriver.Chrome): Selenium Chrome Driver
+
+    Returns:
+        None
+    """
+
+    global set_once
+
+    # Load Gulper.io
+    driver.get("https://gulper.io/")  # Load Gulper.io
+    time.sleep(3)  # Wait for page to load
+
+    # Find Web Body in Driver
+    body = driver.find_element(By.TAG_NAME, "body")
+    body_width = body.size['width']
+    body_height = body.size['height']
+
+    # Set Settings for Once
+    if not set_once:
+        driver.execute_script("document.getElementById('show-lbrd').click()")
+        driver.execute_script("document.getElementById('show-nicks').click()")
+        driver.execute_script("document.getElementById('show-map').click()")
+        driver.execute_script("document.getElementById('graph-qual').click()")
+        driver.execute_script("document.getElementById('framerate').click()")
+        set_once = True
+        time.sleep(0.25)
+
+    # Hide Ads
+    driver.execute_script("window.ADS_BLOCKED = true;")
+
+    # Hide Stats
+    driver.execute_script(
+        "document.getElementById('stats').style.display = 'none'")
+
+    # Wait for 0.25 seconds
+    time.sleep(0.25)
+
+    # Press Tab Key
+    body.send_keys(Keys.TAB)
+    time.sleep(0.5)
+
+    # Type Nickname
+    random_names = []
+    random_names.append("GULPY_BOT_" + str(random.randint(100, 9999)))
+
+    input_box = driver.switch_to.active_element
+    input_box.send_keys(random_names[random.randint(0, len(random_names) - 1)])
+
+    # Wait for 1 second
+    time.sleep(1)
+
+    # Find Element By Id [ start-btn ] and Click
+    start_btn = driver.find_element(By.ID, "start-btn")
+    start_btn.click()
+
+    # Wait for 1 second
+    time.sleep(5)
+
+    # Step Counter
+    step = 0
+
+    driver.execute_script(
+        "document.getElementById('stats').style.display = 'none'")
+
+    while True:
+        step += 1
+        # Check If Elelment by Id [ game-stats ] is Visible
+        game_stats = driver.find_element(By.ID, "game-stats")
+        if game_stats.is_displayed():
+            # Game Over
+            break
+
+        try:
+            # Hover Mouse in Random Position
+            position_x = random.randint(-1 *
+                                        int(body_width/2)+10, int(body_width/2)-10)
+
+            position_y = random.randint(-1 * int(body_height/2) +
+                                        10, int(body_height/2)-10)
+
+            action_chains.move_to_element(body).move_by_offset(
+                position_x, position_y).perform()
+            print("Moving to: " + str(position_x) + ", " + str(position_y))
+
+            time.sleep(0.1)
+            action_chains.move_to_element(body).move_by_offset(0, 0).perform()
+        except Exception as e:
+            # Print Error
+            print(e, position_x, position_y)
+
+            # Wait for 0.25 seconds
+            time.sleep(0.1)
+
+        if step % 10 == 0:
+            # Try to Remove Stats
+            driver.execute_script(
+                "document.getElementById('stats').style.display = 'none'")
+
+    # Restart Game
+    the_gulper_bot(driver)
+
+
+# Run the Gulper Bot
+the_gulper_bot(driver)
